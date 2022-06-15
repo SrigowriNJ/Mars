f5c68354e03096bf4f63d1245c7505a4ab89f173
SrigowriNJ <srigowrinj@gmail.com>
2022-06-15 08:16:24 -0500

    Assignment commit

--- JS/demo.js
+++ JS/demo.js
@@ -1,3 +1,4 @@
+/*
 let num1=123456
 let sum=0
 for(i=0;i<6;i+=2){
@@ -5,3 +6,18 @@ sum=sum+num1%10
 num1=parseInt(num1/10)
 }
 console.log(sum)
+*/
+
+const employee={
+    empID:'',
+    empName:'',
+    address:'',
+    organisation:'',
+    role:{
+        withinTeam:'',
+        withinOrganisation:'',
+
+    }
+}
+
+let display=({empName,empID,withinTeam}) => console.log()
